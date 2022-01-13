84ee28abf679a5d2897c207fda3f7803087ddb0b
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-13 21:03:23 +0900

    重複したエラー表示をしないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,           presence: true
-  validates :first_name_kanji,   presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kanji,   presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :last_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
   validates :birth_date,         presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true
 end
