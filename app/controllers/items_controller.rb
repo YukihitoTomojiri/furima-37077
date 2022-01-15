6b7baa6a1ff1c54fa380b0da08793b9a0d4c2ea6
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-15 16:31:26 +0900

    商品出品ページが表示されるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,23 @@ class ItemsController < ApplicationController
   def index
 
   end
-  
-  def new
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item,:comment,:category_id,:status_id,:postage_type_id,:area_id,:preparation_day_id,:price).merge(user_id: current_user.id)
   end
 
   # def move_to_index
