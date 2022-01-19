fd0a5e03d97f000ce8e84af37991f50bf18dc6ed
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-19 21:13:52 +0900

    itemモデルのバリデーションが動作するように改善

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :item_category
+  belongs_to_active_hash :category
   belongs_to_active_hash :item_status
   belongs_to_active_hash :item_postage_type
   belongs_to_active_hash :item_area
@@ -11,11 +11,10 @@ class Item < ApplicationRecord
     validates :image
     validates :item
     validates :comment
-    validates :category_id,        numericality: { other_then: 1 , message: "can't be Blank"}
-    validates :status_id,          numericality: { other_then: 1 , message: "can't be Blank"}
-    validates :area_id,            numericality: { other_then: 1 , message: "can't be Blank"}
-    validates :preparation_day_id, numericality: { other_then: 1 , message: "can't be Blank"}
+    validates :category_id,        numericality: { other_than: 1 , message: "can't be Blank"}
+    validates :status_id,          numericality: { other_than: 1 , message: "can't be Blank"}
+    validates :area_id,            numericality: { other_than: 1 , message: "can't be Blank"}
+    validates :preparation_day_id, numericality: { other_than: 1 , message: "can't be Blank"}
     validates :price,              numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
   end
-  
 end
