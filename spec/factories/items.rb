a499b130b321152ea9d7257146a91011d7092057
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-21 21:41:47 +0900

    items.rbの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :item do
-    image              {Faker::Lorem.sentence}
-    item               { "カフェラテ" }
-    comment            {Faker::Lorem.sentence}
-    category_id        { 2 }
-    status_id          { 2 }
-    area_id            { 2 }
-    preparation_day_id { 2 }
-    price              { 1000 }
+    item                 { "カフェラテ" }
+    comment              {Faker::Lorem.sentence}
+    category_id          { 2 }
+    postage_type_id      { 2 }
+    status_id            { 2 }
+    area_id              { 2 }
+    preparation_day_id   { 2 }
+    price                { 1000 }
+
+    association          :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
