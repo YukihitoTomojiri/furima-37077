aa3a88619d090d1ab5a798a756a1c2bc43efaa0e
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-21 20:44:15 +0900

    itemsモデルのテストコードを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    image              {Faker::Lorem.sentence}
+    item               { "カフェラテ" }
+    comment            {Faker::Lorem.sentence}
+    category_id        { 2 }
+    status_id          { 2 }
+    area_id            { 2 }
+    preparation_day_id { 2 }
+    price              { 1000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
