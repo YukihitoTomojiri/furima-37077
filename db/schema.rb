e9ea303b9a443c35bac5e9aad8300f165d0eaa31
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-22 18:45:27 +0900

    マイグレーションファイルをコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -48,15 +48,6 @@ ActiveRecord::Schema.define(version: 2022_01_22_084808) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_purchases_on_item_id"
-    t.index ["user_id"], name: "index_purchases_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
