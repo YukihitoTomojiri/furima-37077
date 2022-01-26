04acef288337ebdf50c42ab586b545e32415f96f
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-26 22:08:46 +0900

    orderコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/create'
+  get 'order/index'
+  get 'order/create'
   devise_for :users
   root to: 'items#index'
   resources :items
