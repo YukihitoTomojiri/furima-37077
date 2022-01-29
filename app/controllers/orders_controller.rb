c44697eb3ee61566d1943c0f15bc016bb2b482e3
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-01-29 16:01:20 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+
+  end
+
+  private
+  def order_params
+    params.(:order_address).permit(:postal_code, :area_id, :municipality, :block, :building, :phone_number).merge(user_id: current_user.id)
   end
 end
