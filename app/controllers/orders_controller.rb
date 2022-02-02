79ac5016ba586dcc6ab235fd2fa022017f2d0c41
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-02-02 22:21:28 +0900

    決済処理の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,21 +7,29 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
       @item = Item.find(params[:item_id])
       render :index
-
     end
 
   end
 
   private
   def order_params
-    params.require(:order_address).permit(:postal_code, :area_id, :municipality, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :area_id, :municipality, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,15 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipality, :block, :building, :phone_number, :order
+  attr_accessor :user_id, :item_id, :price, :postal_code, :area_id, :municipality, :block, :building, :phone_number, :order, :token
   with_options presence: true do
-    validates :user_id
-    validates :item_id
+    validates :token
     validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :municipality
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short."}, numericality: {only_integer: true, message: "is invalid. Input only number"}
   end
-  validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
